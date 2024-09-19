ca53cf76b0da1ea0b1ba257d69a744209ff0e99e
Jack Vial <vialjack@gmail.com>
2024-09-18 21:35:55 -0400

    readme

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-clang++ -o xornn main.cpp -std=c++11 -lm
+`clang++ -o xornn main.cpp -std=c++11 -lm`
+
+```
+$ ./xornn 
+Input: 0 0 Output: 0.0168766
+Input: 0 1 Output: 0.980629
+Input: 1 0 Output: 0.980629
+Input: 1 1 Output: 0.0184401
+```
